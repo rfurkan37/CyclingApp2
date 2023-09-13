bce666a570a4511ac24d3bf2318f93051d4ebe0b
rfurkan37 <rfurkan37@gmail.com>
2023-09-13 16:55:00 +0300

    location based speed came

--- app/src/main/java/com/example/cyclingapp2/MainActivity.java
+++ app/src/main/java/com/example/cyclingapp2/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.cyclingapp2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.os.SystemClock;
 import android.os.TestLooperManager;
 import android.view.View;
@@ -15,42 +27,48 @@ import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.Toast;
-import java.lang.Double;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.lang.Double;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
     private Chronometer chronometer;
+    private TextView speedTextView;
     private long pauseOffset;
     private boolean running;
     private long velocity = 0;
+
+    LocationManager locationManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        chronometer = (Chronometer)findViewById(R.id.chronometer);
+        chronometer = (Chronometer) findViewById(R.id.chronometer);
+        speedTextView = findViewById(R.id.textViewSpeed);
+
         SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
-        if(sensorManager != null){
+        if (sensorManager != null) {
             Sensor acceleroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-            if(acceleroSensor != null){
-                sensorManager.registerListener(this,acceleroSensor,SensorManager.SENSOR_DELAY_NORMAL);
+            if (acceleroSensor != null) {
+                sensorManager.registerListener(this, acceleroSensor, SensorManager.SENSOR_DELAY_NORMAL);
             }
+        } else {
+            Toast.makeText(this, "Accelero sensor not detected.", Toast.LENGTH_SHORT).show();
         }
 
-        else {
-            Toast.makeText(this,"Accelero sensor not detected.",Toast.LENGTH_SHORT).show();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
         }
-    }
-    public void editVelocity() {
-        TextView velocity = (TextView)findViewById(R.id.textViewSpeed);
-        TextView acc = (TextView)findViewById(R.id.textViewAcceleration);
 
-        final double [] accele = new double[1];
-        final double [] time = new double[1];
-
-        time[0] = (SystemClock.elapsedRealtime()-chronometer.getBase()) / 1000;
-        accele[0] = Double.parseDouble(acc.getText().toString());
-        velocity.setText("ms" + time[0]*accele[0]);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0 , 0, this);
     }
     public void startChronometer(View v) {
         if(!running){
@@ -70,17 +88,43 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         chronometer.setBase(SystemClock.elapsedRealtime());
         pauseOffset = 0;
     }
+
+
+
     @SuppressLint("SetTextI18n")
     @Override
     public void onSensorChanged (SensorEvent event) {
         TextView accelerationview = (TextView)findViewById(R.id.textViewAcceleration);
 
         if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            accelerationview.setText(String.format("%f", event.values[0]));
-            if (running) editVelocity();
+            accelerationview.setText(String.format("%.2f", event.values[0]));
+            
         }
     }
     @Override
     public void onAccuracyChanged(Sensor sensor,int accuracy){
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        float speed = location.getSpeed();
+
+
+        speedTextView.setText(String.format("%.2f km/h", speed));
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
     }
 }
--- app/src/main/java/com/example/cyclingapp2/SlopeCalculator.java
+++
@@ -1,6 +0,0 @@
-package com.example.cyclingapp2;
-
-public class SlopeCalculator {
-
-
-}
